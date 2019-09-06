71c47b9790cc00ce98dcc9551f461ebb79a6a2c6
黄文强 <viphwq@163.com>
2019-09-06 10:24:59 +0800

    1...

---
+++ mylibrary/src/main/java/com/yanhua/mylibrary/HelloWord.java
@@ -0,0 +1,18 @@
+package com.yanhua.mylibrary;
+
+import android.util.Log;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * A simple {@link HelloWord} subclass.
+ * Created by WenqiangHuang
+ * Creation time 2019/9/6
+ * Email viphwq@163.com
+ */
+public class HelloWord {
+    public void log(String string){
+        Log.e(TAG, "log: "+string );
+    }
+
+}
